1dcc5774fea9ff21fa7b7f57289c99f6b962e0ea
christiandendulay <christian.den.dulay@gmail.com>
2020-02-04 15:11:42 +0800

    Addres Fix

--- server/src/core/supplier/supplier.ts
+++ server/src/core/supplier/supplier.ts
@@ -1,26 +1,23 @@
-import { TAddress, createMakeAddress } from '../address/address';
+import { TAddress, createMakeAddress } from "../address/address";
 
 export type TSupplier = {
-	name: string;
-	address: TAddress;
+  name: string;
+  address: string;
 };
 
 const createMakeSupplier = () => (supplier): TSupplier => {
-	const { name, address } = supplier;
-	const makeAddress = createMakeAddress();
-	if (!name) {
-		throw new Error('Supplier name is required.');
-	}
-	if (!address) {
-		throw new Error('Address is required');
-	}
+  const { name, address } = supplier;
+  if (!name) {
+    throw new Error("Supplier name is required.");
+  }
+  if (!address) {
+    throw new Error("Address is required");
+  }
 
-	const _address = makeAddress(address);
-
-	return {
-		name: name,
-		address: _address,
-	};
+  return {
+    name: name,
+    address: address
+  };
 };
 
 export { createMakeSupplier };
