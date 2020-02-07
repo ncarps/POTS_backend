149b70a06906cbd86b3999714aae143b2ce48339
nat <nsebcarps09@gmail.com>
2020-02-08 00:29:22 +0800

    item typeDefs

---
+++ server/src/graphql/typeDefs/item/index.ts
@@ -0,0 +1,45 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+	type Item {
+		id: ID!
+		itemNo: String!
+		description: String!
+		quantity: String!
+		uom: String!
+		price: String!
+		currency: String!
+	}
+
+	type Query {
+		item(id: String): Item
+		allItems: [Item]
+	}
+
+	type Mutation {
+		createItem(item: ItemInput): Item
+		updateItem(item: UpdateItemInput): Item
+		deleteItem(id: ID!): Item
+	}
+
+	input ItemInput {
+		itemNo: String!
+		description: String!
+		quantity: String!
+		uom: String!
+		price: String!
+		currency: String!
+	}
+
+	input UpdateItemInput {
+		id: ID!
+		itemNo: String!
+		description: String!
+		quantity: String!
+		uom: String!
+		price: String!
+		currency: String!
+	}
+`;
+
+export default typeDefs;
