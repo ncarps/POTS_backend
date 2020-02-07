be211145f75cb52b689162f3cddbcf47902c58d5
nat <nsebcarps09@gmail.com>
2020-02-08 00:12:37 +0800

    Add item binding

---
+++ server/src/models/bindings/item-binding-mongo.ts
@@ -0,0 +1,89 @@
+import { IDBModel } from '../../commons/types';
+import { Item } from '../mongo-models';
+
+const itemModel: IDBModel<any> = {
+	insert: async item => {
+		const newItem = await new Item({
+			itemNo: item.itemNo,
+			description: item.description,
+			quantity: item.quantity,
+			uom: item.uom,
+			price: item.price,
+			currency: item.currency,
+		});
+
+		return new Promise((resolve, reject) => {
+			newItem.save((err, res) => {
+				err ? reject(err) : resolve(res);
+			});
+		});
+	},
+
+	getById: async id => {
+		const item: any = await Item.findOne({ _id: id }).exec();
+		if (!item._id) {
+			throw new Error('No item found');
+		}
+		return {
+			id: item._id.toString(),
+			itemNo: item.itemNo,
+			description: item.description,
+			quantity: item.quantity,
+			uom: item.uom,
+			price: item.price,
+			currency: item.currency,
+		};
+	},
+
+	getAll: async () => {
+		const item: any = await Item.find({}).exec();
+
+		return item.map(i => ({
+			id: i._id.toString(),
+			itemNo: i.itemNo,
+			description: i.description,
+			quantity: i.quantity,
+			uom: i.uom,
+			price: i.price,
+			currency: i.currency,
+		}));
+	},
+
+	deleteById: async id => {
+		return new Promise((resolve, reject) => {
+			Item.findByIdAndDelete(id).exec((err, res) => {
+				err ? reject(err) : resolve(res);
+			});
+		});
+	},
+
+	updateById: async data => {
+		const item: any = await Item.findByIdAndUpdate(
+			{
+				_id: data.id,
+			},
+			{
+				itemNo: data.itemNo,
+				description: data.description,
+				quantity: data.quantity,
+				uom: data.uom,
+				price: data.price,
+				currency: data.currency,
+			},
+			{
+				new: true,
+			}
+		).exec();
+		return {
+			id: item._id,
+			itemNo: item.itemNo,
+			description: item.description,
+			quantity: item.quantity,
+			uom: item.uom,
+			price: item.price,
+			currency: item.currency,
+		};
+	},
+};
+
+export { itemModel };
--- server/src/models/mongo-models/index.ts
+++ server/src/models/mongo-models/index.ts
@@ -2,5 +2,6 @@ import User from './User';
 import Address from './Address';
 import Supplier from './Supplier';
 import SupplierStatus from './SupplierStatus';
+import Item from './Item';
 
-export { User, Address, Supplier, SupplierStatus };
+export { User, Address, Supplier, SupplierStatus, Item };
