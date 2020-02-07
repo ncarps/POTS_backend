d8d8dc57ee81bb3dfbb28e9e9b68854c76f55e13
nat <nsebcarps09@gmail.com>
2020-02-07 15:20:12 +0800

    Add create-item controller and tests

---
+++ server/src/controllers/create-item-db.ts
@@ -0,0 +1,10 @@
+import { IDBModel } from '../commons/types';
+import { createUCCreateItem } from '../core/';
+
+const createCreateItemDB = (db: IDBModel<any>) => async i => {
+	const createItem = createUCCreateItem();
+	const newItem = createItem(i);
+	return db.insert(newItem);
+};
+
+export { createCreateItemDB };
