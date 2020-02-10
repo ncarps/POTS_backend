7ccbeed90549744d8df0878d288274449c28aeb8
nat <nsebcarps09@gmail.com>
2020-02-10 16:49:02 +0800

    Add PurchaseOrders model

---
+++ server/src/models/mongo-models/PurchaseOrder.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import { ObjectID } from 'mongodb';
+
+const Schema = mongoose.Schema;
+
+ObjectID.prototype.valueOf = function() {
+	return this.toString();
+};
+
+const PurchaseOrderSchema = new Schema({
+	externalID: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		required: true,
+	},
+	supplierStatus: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+	supplier: {
+		type: String,
+		required: true,
+	},
+	items: {
+		type: [String],
+		required: true,
+	},
+});
+
+export default mongoose.model('PurchaseOrder', PurchaseOrderSchema);
--- server/src/models/mongo-models/index.ts
+++ server/src/models/mongo-models/index.ts
@@ -3,5 +3,6 @@ import Address from './Address';
 import Supplier from './Supplier';
 import SupplierStatus from './SupplierStatus';
 import Item from './Item';
+import PurchaseOrder from './PurchaseOrder';
 
-export { User, Address, Supplier, SupplierStatus, Item };
+export { User, Address, Supplier, SupplierStatus, Item, PurchaseOrder };
