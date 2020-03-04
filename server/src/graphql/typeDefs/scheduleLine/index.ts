925166ae6efe546eac6529eff720de260839fcae
christiandendulay <christian.den.dulay@gmail.com>
2020-03-04 14:52:35 +0800

    Fix CreatePO and CreateScheduleLines

--- server/src/graphql/typeDefs/scheduleLine/index.ts
+++ server/src/graphql/typeDefs/scheduleLine/index.ts
@@ -1,45 +1,45 @@
-import { gql } from 'apollo-server';
+import { gql } from "apollo-server";
 
 const typeDefs = gql`
-	type ScheduleLine {
-		id: ID!
-		quantity: Float!
-		uom: String!
-		unitPrice: Float!
-		totalAmount: Float!
-		deliveryDateAndTime: String!
-		deliveryStatus: [SupplierStatus]
-	}
+  type ScheduleLine {
+    id: ID!
+    quantity: Float!
+    uom: String!
+    unitPrice: Float!
+    totalAmount: Float!
+    deliveryDateAndTime: String!
+    deliveryStatus: [SupplierStatus]
+  }
 
-	type Query {
-		scheduleLine(id: String): ScheduleLine
-		allScheduleLines: [ScheduleLine]
-	}
+  type Query {
+    scheduleLine(id: String): ScheduleLine
+    allScheduleLines: [ScheduleLine]
+  }
 
-	type Mutation {
-		createScheduleLine(scheduleLine: ScheduleLineInput!): ScheduleLine
-		updateScheduleLine(scheduleLine: UpdateScheduleLineInput!): ScheduleLine
-		deleteScheduleLine(id: ID!): ScheduleLine
-	}
+  type Mutation {
+    createScheduleLine(scheduleLine: ScheduleLineInput!): ScheduleLine
+    updateScheduleLine(scheduleLine: UpdateScheduleLineInput!): ScheduleLine
+    deleteScheduleLine(id: ID!): ScheduleLine
+  }
 
-	input ScheduleLineInput {
-		quantity: Float!
-		uom: String!
-		unitPrice: Float!
-		totalAmount: Float!
-		deliveryDateAndTime: String!
-		deliveryStatus: SupplierStatusInput
-	}
+  input ScheduleLineInput {
+    quantity: Float!
+    uom: String!
+    unitPrice: Float!
+    totalAmount: Float!
+    deliveryDateAndTime: String!
+    deliveryStatus: [SupplierStatusInput]
+  }
 
-	input UpdateScheduleLineInput {
-		id: ID!
-		quantity: Float
-		uom: String
-		unitPrice: Float
-		totalAmount: Float
-		deliveryDateAndTime: String
-		deliveryStatus: SupplierStatusInput
-	}
+  input UpdateScheduleLineInput {
+    id: ID!
+    quantity: Float
+    uom: String
+    unitPrice: Float
+    totalAmount: Float
+    deliveryDateAndTime: String
+    deliveryStatus: SupplierStatusInput
+  }
 `;
 
 export default typeDefs;
