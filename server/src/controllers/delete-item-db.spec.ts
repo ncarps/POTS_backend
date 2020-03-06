3fcc8872419743fbf14d81fd6ed94f575e9e7070
nat <nsebcarps09@gmail.com>
2020-03-06 17:25:03 +0800

    update delete controllers

--- server/src/controllers/delete-item-db.spec.ts
+++ server/src/controllers/delete-item-db.spec.ts
@@ -1,44 +1,60 @@
-// import { DeleteRecordByIDDB } from './delete-data-db';
+import { DeleteRecordByIDDB } from './delete-data-db';
 
-// const mockItem = [
-// 	{
-// 		_id: '1',
-// 		itemNo: '1',
-// 		description: 'Corned Beef',
-// 		quantity: '5',
-// 		uom: 'kg',
-// 		price: '2000',
-// 		currency: 'PHP',
-// 	},
-// 	{
-// 		_id: '2',
-// 		itemNo: '2',
-// 		description: 'Burger',
-// 		quantity: '6',
-// 		uom: 'kg',
-// 		price: '3000',
-// 		currency: 'USD',
-// 	},
-// ];
+const mockItem = [
+	{
+		_id: '1',
+		itemNo: '001',
+		description: 'Beef',
+		productId: '0001',
+		quantity: '10',
+		uom: 'kg',
+		unitPrice: '1000',
+		discount: '10%',
+		totalAmount: '9000',
+		supplierStatusItem: 'Distpatched',
+		deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+		scheduleLine: { quantity: '', uom: 'uom', deliveryDate: 'deliveryDate', supplerStatus: 'supplerStatus' },
+		currency: 'currency',
+		dateUpdated: '',
+		timeUpdated: '',
+	},
+	{
+		_id: '2',
+		itemNo: '002',
+		description: 'Beef',
+		productId: '0001',
+		quantity: '10',
+		uom: 'kg',
+		unitPrice: '1000',
+		discount: '10%',
+		totalAmount: '9000',
+		supplierStatusItem: 'Distpatched',
+		deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+		scheduleLine: { quantity: '', uom: 'uom', deliveryDate: 'deliveryDate', supplerStatus: 'supplerStatus' },
+		currency: 'currency',
+		dateUpdated: '',
+		timeUpdated: '',
+	},
+];
 
-// describe('Delete Item', () => {
-// 	it('Should be able to delete an item by ID in DB', async () => {
-// 		const oneData = [mockItem[0]];
-// 		const mockDB: any = {
-// 			deleteById: jest.fn(async id => {
-// 				const filterMock = data => {
-// 					if (data._id === id) {
-// 						return data;
-// 					}
-// 				};
+describe('Delete Item', () => {
+	it('Should be able to delete an item by ID in DB', async () => {
+		const oneData = [mockItem[0]];
+		const mockDB: any = {
+			deleteById: jest.fn(async id => {
+				const filterMock = data => {
+					if (data._id === id) {
+						return data;
+					}
+				};
 
-// 				return mockItem.filter(filterMock);
-// 			}),
-// 		};
-// 		const getOneData = DeleteRecordByIDDB(mockDB);
-// 		const data = await getOneData('1');
-// 		expect(mockDB.deleteById.mock.calls.length).toBe(1);
+				return mockItem.filter(filterMock);
+			}),
+		};
+		const getOneData = DeleteRecordByIDDB(mockDB);
+		const data = await getOneData('1');
+		expect(mockDB.deleteById.mock.calls.length).toBe(1);
 
-// 		expect(data).toMatchObject(oneData);
-// 	});
-// });
+		expect(data).toMatchObject(oneData);
+	});
+});
--- server/src/controllers/delete-purchaseOrder-db.spec.ts
+++ server/src/controllers/delete-purchaseOrder-db.spec.ts
@@ -1,86 +1,130 @@
-// import { DeleteRecordByIDDB } from './delete-data-db';
+import { DeleteRecordByIDDB } from './delete-data-db';
 
-// const mockPurchaseOrder = [
-// 	{
-// 		_id: '1',
-// 		externalID: '001',
-// 		status: 'Pending',
-// 		supplierStatus: {
-// 			_id: '1',
-// 			status: 'Dispatched',
-// 			dateCreated: 'February 14, 2020',
-// 		},
-// 		supplier: {
-// 			_id: '1',
-// 			name: 'Supplier Name-1',
-// 			address: {
-// 				building_name: 'building 1',
-// 				city: 'city 1',
-// 				street: 'street 1',
-// 				zip_code: '123',
-// 			},
-// 		},
-// 		items: [
-// 			{
-// 				itemNo: '1',
-// 				description: 'Corned Beef',
-// 				quantity: '5',
-// 				uom: 'kg',
-// 				price: '2000',
-// 				currency: 'PHP',
-// 			},
-// 		],
-// 	},
-// 	{
-// 		_id: '2',
-// 		externalID: '002',
-// 		status: 'Shipped',
-// 		supplierStatus: {
-// 			_id: '1',
-// 			status: 'Dispatched',
-// 			dateCreated: 'February 14, 2020',
-// 		},
-// 		supplier: {
-// 			_id: '1',
-// 			name: 'Supplier Name-1',
-// 			address: {
-// 				building_name: 'building 1',
-// 				city: 'city 1',
-// 				street: 'street 1',
-// 				zip_code: '123',
-// 			},
-// 		},
-// 		items: [
-// 			{
-// 				itemNo: '1',
-// 				description: 'Corned Beef',
-// 				quantity: '5',
-// 				uom: 'kg',
-// 				price: '2000',
-// 				currency: 'PHP',
-// 			},
-// 		],
-// 	},
-// ];
+const mockPurchaseOrder = [
+	{
+		_id: '1',
+		purchaseOrderNo: '001',
+		shipmentNo: '123',
+		adminStatus: 'status',
+		supplierStatusHeader: 'supplierStatus',
+		supplier: {
+			supplierNo: '001',
+			supplierName: 'Juan Dela Cruz',
+			address: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+			contactPerson: 'Basil Valdez',
+			contactNumber: '1234567',
+			tin: '12345',
+		},
+		postingDate: '03/03/20200',
+		documentDate: '03/03/20200',
+		vendorAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+		items: [
+			{
+				itemNo: '001',
+				description: 'Beef',
+				productId: '0001',
+				quantity: '10',
+				uom: 'kg',
+				unitPrice: '1000',
+				discount: '10%',
+				totalAmount: '9000',
+				supplierStatusItem: 'Distpatched',
+				deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+				scheduleLine: '',
+				currency: 'currency',
+				dateUpdated: '',
+				timeUpdated: '',
+			},
+			{
+				itemNo: '002',
+				description: 'Beef',
+				productId: '0001',
+				quantity: '10',
+				uom: 'kg',
+				unitPrice: '1000',
+				discount: '10%',
+				totalAmount: '9000',
+				supplierStatusItem: 'Distpatched',
+				deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+				scheduleLine: '',
+				currency: 'currency',
+				dateUpdated: '',
+				timeUpdated: '',
+			},
+		],
+	},
+	{
+		_id: '2',
+		purchaseOrderNo: '002',
+		shipmentNo: '123',
+		adminStatus: 'status',
+		supplierStatusHeader: 'supplierStatus',
+		supplier: {
+			supplierNo: '001',
+			supplierName: 'Juan Dela Cruz',
+			address: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+			contactPerson: 'Basil Valdez',
+			contactNumber: '1234567',
+			tin: '12345',
+		},
+		postingDate: '03/03/20200',
+		documentDate: '03/03/20200',
+		vendorAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+		items: [
+			{
+				itemNo: '001',
+				description: 'Beef',
+				productId: '0001',
+				quantity: '10',
+				uom: 'kg',
+				unitPrice: '1000',
+				discount: '10%',
+				totalAmount: '9000',
+				supplierStatusItem: 'Distpatched',
+				deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+				scheduleLine: '',
+				currency: 'currency',
+				dateUpdated: '',
+				timeUpdated: '',
+			},
+			{
+				itemNo: '002',
+				description: 'Beef',
+				productId: '0001',
+				quantity: '10',
+				uom: 'kg',
+				unitPrice: '1000',
+				discount: '10%',
+				totalAmount: '9000',
+				supplierStatusItem: 'Distpatched',
+				deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+				scheduleLine: '',
+				currency: 'currency',
+				dateUpdated: '',
+				timeUpdated: '',
+			},
+		],
+	},
+];
 
-// describe('Delete purchase order', () => {
-// 	it('Should be able to delete a pruchase order by ID in DB', async () => {
-// 		const oneData = [mockPurchaseOrder[0]];
-// 		const mockDB: any = {
-// 			deleteById: jest.fn(async id => {
-// 				const filterMock = data => {
-// 					if (data._id === id) {
-// 						return data;
-// 					}
-// 				};
+describe('Delete purchase order', () => {
+	it('Should be able to delete a pruchase order by ID in DB', async () => {
+		const oneData = [mockPurchaseOrder[0]];
+		const mockDB: any = {
+			deleteById: jest.fn(async id => {
+				const filterMock = data => {
+					if (data._id === id) {
+						return data;
+					}
+				};
 
-// 				return mockPurchaseOrder.filter(filterMock);
-// 			}),
-// 		};
-// 		const getOneData = DeleteRecordByIDDB(mockDB);
-// 		const data = await getOneData('1');
-// 		expect(mockDB.deleteById.mock.calls.length).toBe(1);
+				return mockPurchaseOrder.filter(filterMock);
+			}),
+		};
+		const getOneData = DeleteRecordByIDDB(mockDB);
+		const data = await getOneData('1');
+		expect(mockDB.deleteById.mock.calls.length).toBe(1);
 
-// 		expect(data).toMatchObject(oneData);
-// 	});
-// });
+		expect(data).toMatchObject(oneData);
+	});
+});
