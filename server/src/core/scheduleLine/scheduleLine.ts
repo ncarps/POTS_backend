8b740a13503c7c6fe2ff03a03159ffd7a3a58f8d
nat <nsebcarps09@gmail.com>
2020-03-06 12:37:42 +0800

    update scheduleLine entity

--- server/src/core/scheduleLine/scheduleLine.ts
+++ server/src/core/scheduleLine/scheduleLine.ts
@@ -11,7 +11,21 @@ export type TScheduleLine = {
 
 const createMakeScheduleLine = () => (scheduleLine): TScheduleLine => {
 	const { quantity, uom, deliveryDateAndTime, deliveryStatus, unitPrice, totalAmount } = scheduleLine;
-
+	if (!quantity) {
+		throw new Error('Quantity is required.');
+	}
+	if (!uom) {
+		throw new Error('UOM is required.');
+	}
+	if (!deliveryDateAndTime) {
+		throw new Error('Delivery Date and Time is required.');
+	}
+	if (!unitPrice) {
+		throw new Error('Unit Price is required.');
+	}
+	if (!totalAmount) {
+		throw new Error('Total Amount is required.');
+	}
 	return {
 		quantity: quantity,
 		uom: uom,
