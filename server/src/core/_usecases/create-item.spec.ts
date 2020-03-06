a6b1ee550e2a143b32c4131cf01494a16cce8d7a
nat <nsebcarps09@gmail.com>
2020-03-06 15:35:39 +0800

    Update purchaseOrder create test

--- server/src/core/_usecases/create-item.spec.ts
+++ server/src/core/_usecases/create-item.spec.ts
@@ -1,18 +1,26 @@
-// import { createUCCreateItem } from './create-item-usecase';
+import { createUCCreateItem } from './create-item-usecase';
 
-// const addItem = createUCCreateItem();
+const addItem = createUCCreateItem();
 
-// describe('Item CRUD', () => {
-// 	it('should be able to create an item', () => {
-// 		const itemInput = {
-// 			itemNo: '1',
-// 			description: 'Corned Beef',
-// 			quantity: '5',
-// 			uom: 'kg',
-// 			price: '2000',
-// 			currency: 'PHP',
-// 		};
-// 		const newItem = addItem(itemInput);
-// 		expect(newItem).toMatchObject(itemInput);
-// 	});
-// });
+describe('Item CRUD', () => {
+	it('should be able to create an item', () => {
+		const itemInput = {
+			itemNo: '001',
+			description: 'Beef',
+			productId: '0001',
+			quantity: '10',
+			uom: 'kg',
+			unitPrice: '1000',
+			discount: '10%',
+			totalAmount: '9000',
+			supplierStatusItem: 'Distpatched',
+			deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+			scheduleLine: { quantity: 'quantity', uom: 'uom', deliveryDate: 'deliveryDate', supplerStatus: 'supplerStatus' },
+			currency: 'currency',
+			dateUpdated: '',
+			timeUpdated: '',
+		};
+		const newItem = addItem(itemInput);
+		expect(newItem).toMatchObject(itemInput);
+	});
+});
--- server/src/core/_usecases/create-purchaseOrder.spec.ts
+++ server/src/core/_usecases/create-purchaseOrder.spec.ts
@@ -1,27 +1,46 @@
-// import { createUCPurchaseOrder } from './create-purchaseOrder-usecase';
+import { createUCPurchaseOrder } from './create-purchaseOrder-usecase';
 
-// const addPurchaseOrder = createUCPurchaseOrder();
+const addPurchaseOrder = createUCPurchaseOrder();
 
-// describe('Purchase Order Create', () => {
-// 	it('should create a purchase order', () => {
-// 		const purchaseOrderInput = {
-// 			externalID: '001',
-// 			status: 'Pending',
-// 			supplierStatus: ['Dispatched'],
-// 			supplier: 'Jollibee',
-// 			items: [
-// 				{
-// 					itemNo: '1',
-// 					description: 'Corned Beef',
-// 					quantity: '5',
-// 					uom: 'kg',
-// 					price: '2000',
-// 					currency: 'PHP',
-// 				},
-// 			],
-// 		};
+describe('Purchase Order Create', () => {
+	it('should create a purchase order', () => {
+		const purchaseOrderInput = {
+			purchaseOrderNo: '001',
+			shipmentNo: '123',
+			adminStatus: 'status',
+			supplierStatusHeader: 'supplierStatus',
+			supplier: {
+				supplierNo: '',
+				supplierName: 'Juan Dela Cruz',
+				address: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+				contactPerson: 'Basil Valdez',
+				contactNumber: '1234567',
+				tin: '12345',
+			},
+			postingDate: '03/03/20200',
+			documentDate: '03/03/20200',
+			vendorAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+			items: [
+				{
+					itemNo: '001',
+					description: 'Beef',
+					productId: '0001',
+					quantity: '10',
+					uom: 'kg',
+					unitPrice: '1000',
+					discount: '10%',
+					totalAmount: '9000',
+					supplierStatusItem: 'Distpatched',
+					deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+					scheduleLine: '',
+					currency: 'currency',
+					dateUpdated: '',
+					timeUpdated: '',
+				},
+			],
+		};
 
-// 		const newPurchaseItem = addPurchaseOrder(purchaseOrderInput);
-// 		expect(newPurchaseItem).toMatchObject(purchaseOrderInput);
-// 	});
-// });
+		const newPurchaseItem = addPurchaseOrder(purchaseOrderInput);
+		expect(newPurchaseItem).toMatchObject(purchaseOrderInput);
+	});
+});
