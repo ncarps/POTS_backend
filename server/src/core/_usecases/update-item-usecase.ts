be9f1f65f80f13d1d7f96087b2326c5214361ec0
nat <nsebcarps09@gmail.com>
2020-03-06 16:06:21 +0800

    update updateItem usecase test

--- server/src/core/_usecases/update-item-usecase.ts
+++ server/src/core/_usecases/update-item-usecase.ts
@@ -1,7 +1,32 @@
-import { makeItem } from '../item';
+import { makeItem, TItem } from '../item';
 
 const updateUCItem = () => (itemInput, oldValue) => {
-	return makeItem(itemInput);
+	let newItem: TItem = {
+		itemNo: oldValue.itemNo,
+		productId: oldValue.productId,
+		description: oldValue.description,
+		quantity: oldValue.quantity,
+		uom: oldValue.uom,
+		unitPrice: oldValue.unitPrice,
+		discount: oldValue.discount,
+		totalAmount: oldValue.totalAmount,
+		deliveryAddress: oldValue.deliveryAddress,
+		supplierStatusItem: oldValue.supplierStatusItem,
+		scheduleLine: oldValue.scheduleLine,
+		currency: oldValue.currency,
+		dateUpdated: oldValue.dateUpdated,
+		timeUpdated: oldValue.timeUpdated,
+	};
+
+	for (let prop in itemInput) {
+		if (itemInput[prop]) {
+			newItem[prop] = itemInput[prop];
+		} else {
+			newItem;
+		}
+	}
+	const item = makeItem(newItem);
+	return { ...item, id: oldValue.id };
 };
 
 export { updateUCItem };
--- server/src/core/_usecases/update-item.spec.ts
+++ server/src/core/_usecases/update-item.spec.ts
@@ -1,26 +1,42 @@
-// import { updateUCItem } from './update-item-usecase';
-// const updateItem = updateUCItem();
+import { updateUCItem } from './update-item-usecase';
+const updateItem = updateUCItem();
 
-// describe('UseCase: Update a Item ', () => {
-// 	it('should be able to update an item', () => {
-// 		const itemInput = {
-// 			itemNo: '1',
-// 			description: 'Corned Beef',
-// 			quantity: '5',
-// 			uom: 'kg',
-// 			price: '2000',
-// 			currency: 'PHP',
-// 		};
+describe('UseCase: Update a Item ', () => {
+	it('should be able to update an item', () => {
+		const itemInput = {
+			// itemNo: '002',
+			// description: 'Beef',
+			// productId: '0001',
+			// quantity: '10',
+			// uom: 'kg',
+			// unitPrice: '1000',
+			// discount: '10%',
+			// totalAmount: '9000',
+			supplierStatusItem: 'On Transit',
+			// deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+			// scheduleLine: { quantity: '', uom: 'uom', deliveryDate: 'deliveryDate', supplerStatus: 'supplerStatus' },
+			// currency: 'currency',
+			dateUpdated: '03/03/2020',
+			timeUpdated: '4:30 PM',
+		};
 
-// 		const oldItemInput = {
-// 			itemNo: '2',
-// 			description: 'Corned Beefs',
-// 			quantity: '50',
-// 			uom: 'mg',
-// 			price: '20001',
-// 			currency: 'USD',
-// 		};
-// 		const newItem = updateItem(itemInput, oldItemInput);
-// 		expect(newItem).toMatchObject(itemInput);
-// 	});
-// });
+		const oldItemInput = {
+			itemNo: '001',
+			description: 'Beef',
+			productId: '0001',
+			quantity: '10',
+			uom: 'kg',
+			unitPrice: '1000',
+			discount: '10%',
+			totalAmount: '9000',
+			supplierStatusItem: 'Dispatched',
+			deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+			scheduleLine: { quantity: '', uom: 'uom', deliveryDate: 'deliveryDate', supplerStatus: 'supplerStatus' },
+			currency: 'currency',
+			dateUpdated: '',
+			timeUpdated: '',
+		};
+		const newItem = updateItem(itemInput, oldItemInput);
+		expect(newItem).toMatchObject(itemInput);
+	});
+});
