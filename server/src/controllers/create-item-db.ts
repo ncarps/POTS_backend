5e041bf93d67e667d15def07bc3bffb565b348c8
Odee <odjhey@gmail.com>
2020-03-10 17:56:13 +0800

    update alignment, follow prettier conf

--- server/src/controllers/create-item-db.ts
+++ server/src/controllers/create-item-db.ts
@@ -2,10 +2,9 @@ import { IDBModel } from '../commons/types';
 import { createUCCreateItem } from '../core/';
 
 const createCreateItemDB = (db: IDBModel<any>) => async i => {
-	const createItem = createUCCreateItem();
-	const newItem = createItem(i);
-	console.log('contorller', newItem);
-	return db.insert(newItem);
+  const createItem = createUCCreateItem();
+  const newItem = createItem(i);
+  return db.insert(newItem);
 };
 
 export { createCreateItemDB };
--- server/src/graphql/resolvers/item/index.ts
+++ server/src/graphql/resolvers/item/index.ts
@@ -1,122 +1,125 @@
 const itemResolvers = {
-	Item: {
-		deliveryAddress: async (parent, args, context, info) => {
-			const { getAddressById } = context;
-			return await getAddressById(parent.deliveryAddress);
-		},
-		scheduleLine: async (parent, args, context, info) => {
-			const { getAllScheduleLinesByItem } = context;
+  Item: {
+    deliveryAddress: async (parent, args, context, info) => {
+      const { getAddressById } = context;
+      return await getAddressById(parent.deliveryAddress);
+    },
+    scheduleLine: async (parent, args, context, info) => {
+      const { getAllScheduleLinesByItem } = context;
 
-			return (await getAllScheduleLinesByItem(parent.scheduleLine)) || null;
-		},
-	},
-	Query: {
-		item: async (parent, { id }, { getItemById }, info) => {
-			return getItemById(id);
-		},
+      return (await getAllScheduleLinesByItem(parent.scheduleLine)) || null;
+    },
+  },
+  Query: {
+    item: async (parent, { id }, { getItemById }, info) => {
+      return getItemById(id);
+    },
 
-		allItems: async (parent, args, { getAllItems }, info) => {
-			return getAllItems();
-		},
-	},
-	Mutation: {
-		createItem: async (parent, { item }, context, info) => {
-			const { createItem, createScheduleLine, createSupplierStatus } = context;
+    allItems: async (parent, args, { getAllItems }, info) => {
+      return getAllItems();
+    },
+  },
+  Mutation: {
+    createItem: async (parent, { item }, context, info) => {
+      const { createItem, createScheduleLine, createSupplierStatus } = context;
 
-			const scheduleLine: Array<any> = await Promise.all(
-				item.scheduleLine.map(async sl => {
-					let deliveryStatus;
-					if (sl.deliveryStatus) {
-						deliveryStatus = await Promise.all(
-							sl.deliveryStatus.map(async ds => {
-								const deliveryStatus = await createSupplierStatus(ds);
-								// console.log(deliveryStatus);
-								return deliveryStatus.id.toString();
-							})
-						);
-					}
-					const scheduleLine = {
-						quantity: sl.quantity,
-						uom: sl.uom,
-						unitPrice: sl.unitPrice,
-						totalAmount: sl.totalAmount,
-						deliveryDateAndTime: sl.deliveryDateAndTime,
-						deliveryStatus: deliveryStatus ? deliveryStatus : null,
-					};
-					const itemSl = await createScheduleLine(scheduleLine);
-					return itemSl.id.toString();
-				})
-			);
+      const scheduleLine: Array<any> = await Promise.all(
+        item.scheduleLine.map(async sl => {
+          let deliveryStatus;
+          if (sl.deliveryStatus) {
+            deliveryStatus = await Promise.all(
+              sl.deliveryStatus.map(async ds => {
+                const deliveryStatus = await createSupplierStatus(ds);
+                // console.log(deliveryStatus);
+                return deliveryStatus.id.toString();
+              }),
+            );
+          }
+          const scheduleLine = {
+            quantity: sl.quantity,
+            uom: sl.uom,
+            unitPrice: sl.unitPrice,
+            totalAmount: sl.totalAmount,
+            deliveryDateAndTime: sl.deliveryDateAndTime,
+            deliveryStatus: deliveryStatus ? deliveryStatus : null,
+          };
+          const itemSl = await createScheduleLine(scheduleLine);
+          return itemSl.id.toString();
+        }),
+      );
 
-			console.log('ITEM UPDATE', item);
-			const i = {
-				itemNo: item.itemNo,
-				productId: item.productId,
-				description: item.description,
-				quantity: item.quantity,
-				uom: item.uom,
-				unitPrice: item.unitPrice,
-				totalAmount: item.totalAmount,
-				discount: item.discount,
-				deliveryAddress: item.deliveryAddress,
-				supplierStatusItem: item.supplierStatusItem,
-				scheduleLine: scheduleLine,
-				currency: item.currency,
-				dateUpdated: item.dateUpdated,
-				timeUpdated: item.timeUpdated,
-			};
+      const i = {
+        itemNo: item.itemNo,
+        productId: item.productId,
+        description: item.description,
+        quantity: item.quantity,
+        uom: item.uom,
+        unitPrice: item.unitPrice,
+        totalAmount: item.totalAmount,
+        discount: item.discount,
+        deliveryAddress: item.deliveryAddress,
+        supplierStatusItem: item.supplierStatusItem,
+        scheduleLine: scheduleLine,
+        currency: item.currency,
+        dateUpdated: item.dateUpdated,
+        timeUpdated: item.timeUpdated,
+      };
 
-			return await createItem(i);
-		},
+      return await createItem(i);
+    },
 
-		updateItem: async (parent, { item }, context, info) => {
-			const { updateItemById, createScheduleLine, createSupplierStatus } = context;
+    updateItem: async (parent, { item }, context, info) => {
+      const {
+        updateItemById,
+        createScheduleLine,
+        createSupplierStatus,
+      } = context;
 
-			// const scheduleLine: Array<any> = await Promise.all(
-			// 	item.scheduleLine.map(async sl => {
-			// 		let deliveryStatus;
-			// 		if (sl.deliveryStatus) {
-			// 			deliveryStatus = await createSupplierStatus(sl.deliveryStatus);
-			// 		}
+      // const scheduleLine: Array<any> = await Promise.all(
+      // 	item.scheduleLine.map(async sl => {
+      // 		let deliveryStatus;
+      // 		if (sl.deliveryStatus) {
+      // 			deliveryStatus = await createSupplierStatus(sl.deliveryStatus);
+      // 		}
 
-			// 		const scheduleLine = {
-			// 			quantity: sl.quantity,
-			// 			uom: sl.uom,
-			// 			unitPrice: sl.unitPrice,
-			// 			totalAmount: sl.totalAmount,
-			// 			deliveryDateAndTime: sl.deliveryDateAndTime,
-			// 			deliveryStatus: deliveryStatus ? deliveryStatus : null,
-			// 		};
-			// 		const itemSl = await createScheduleLine(scheduleLine);
-			// 		return itemSl.id.toString();
-			// 	})
-			// );
+      // 		const scheduleLine = {
+      // 			quantity: sl.quantity,
+      // 			uom: sl.uom,
+      // 			unitPrice: sl.unitPrice,
+      // 			totalAmount: sl.totalAmount,
+      // 			deliveryDateAndTime: sl.deliveryDateAndTime,
+      // 			deliveryStatus: deliveryStatus ? deliveryStatus : null,
+      // 		};
+      // 		const itemSl = await createScheduleLine(scheduleLine);
+      // 		return itemSl.id.toString();
+      // 	})
+      // );
 
-			const i = {
-				id: item.id,
-				itemNo: item.itemNo,
-				productId: item.productId,
-				description: item.description,
-				quantity: item.quantity,
-				uom: item.uom,
-				unitPrice: item.unitPrice,
-				totalAmount: item.totalAmount,
-				discount: item.discount,
-				deliveryAddress: item.deliveryAddress,
-				supplierStatusItem: item.supplierStatusItem,
-				scheduleLine: item.scheduleLine,
-				currency: item.currency,
-				dateUpdated: item.dateUpdated,
-				timeUpdated: item.timeUpdated,
-			};
-			return await updateItemById(i);
-		},
+      const i = {
+        id: item.id,
+        itemNo: item.itemNo,
+        productId: item.productId,
+        description: item.description,
+        quantity: item.quantity,
+        uom: item.uom,
+        unitPrice: item.unitPrice,
+        totalAmount: item.totalAmount,
+        discount: item.discount,
+        deliveryAddress: item.deliveryAddress,
+        supplierStatusItem: item.supplierStatusItem,
+        scheduleLine: item.scheduleLine,
+        currency: item.currency,
+        dateUpdated: item.dateUpdated,
+        timeUpdated: item.timeUpdated,
+      };
+      return await updateItemById(i);
+    },
 
-		deleteItem: async (parent, { id }, context, info) => {
-			const { deleteItemById } = context;
-			return await deleteItemById(id);
-		},
-	},
+    deleteItem: async (parent, { id }, context, info) => {
+      const { deleteItemById } = context;
+      return await deleteItemById(id);
+    },
+  },
 };
 
 export default itemResolvers;
