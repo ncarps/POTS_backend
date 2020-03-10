50c7d418b575390057ed68f0531a71f45b711f0c
nat <nsebcarps09@gmail.com>
2020-03-10 11:10:44 +0800

    Add purchaseOrder integ-tests

--- server/__tests__/integ-graphql.test.ts
+++ server/__tests__/integ-graphql.test.ts
@@ -1403,106 +1403,32 @@ describe('Queries', () => {
 	});
 
 	//Item Mutation
-	it('create an item', async () => {
-		const CREATE_ITEM = gql`
-			mutation createI($item: ItemInput!) {
-				createItem(item: $item) {
-					itemNo
-					productId
-					description
-					quantity
-					uom
-					unitPrice
-					discount
-					totalAmount
-					deliveryAddress {
-						building_name
-						zip_code
-						state
-						street
-						city
-					}
-					scheduleLine {
-						quantity
-						uom
-						unitPrice
-						totalAmount
-					}
-					currency
-				}
-			}
-		`;
 
-		const { mutate } = createTestClient(server);
-		const res = await mutate({
-			mutation: CREATE_ITEM,
-			variables: {
-				item: {
-					itemNo: '001',
-					description: 'Beef',
-					productId: '0001',
-					quantity: 10,
-					uom: 'kilograms',
-					unitPrice: 1000,
-					discount: 0.05,
-					totalAmount: 10000,
-					deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
-					scheduleLine: [
-						{
-							quantity: 10,
-							uom: 'kilograms',
-							deliveryDateAndTime: 'February 25,2020 4:30PM',
-							unitPrice: 1000,
-							totalAmount: 10000,
-						},
-					],
-					currency: 'PHP',
-				},
-			},
-		});
-
-		console.log('testtt', res.errors);
-		expect(res.errors).toBeUndefined();
-		expect(itemMock.insert.mock.calls.length).toBe(1);
-
-		expect(res.data).toMatchObject({
-			createItem: {
-				id: '1',
-				itemNo: '001',
-				description: 'Beef',
-				productId: '0001',
-				quantity: 10,
-				uom: 'kilograms',
-				unitPrice: 1000,
-				discount: 0.05,
-				totalAmount: 10000,
-				deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
-				scheduleLine: [
-					{
-						id: '1',
-						quantity: 10,
-						uom: 'kilograms',
-						deliveryDateAndTime: 'February 25,2020 4:30PM',
-						unitPrice: 1000,
-						totalAmount: 10000,
-					},
-				],
-				currency: 'PHP',
-			},
-		});
-		expect(res).toMatchSnapshot();
-	});
-
-	// it('update an item', async () => {
-	// 	const UPDATE_ITEM = gql`
-	// 		mutation i($item: UpdateItemInput!) {
-	// 			updateItem(item: $item) {
-	// 				id
+	// it('should create an item', async () => {
+	// 	const CREATE_ITEM = gql`
+	// 		mutation createI($item: ItemInput!) {
+	// 			createItem(item: $item) {
 	// 				itemNo
+	// 				productId
 	// 				description
 	// 				quantity
 	// 				uom
-	// 				price
+	// 				unitPrice
+	// 				discount
+	// 				totalAmount
+	// 				deliveryAddress {
+	// 					building_name
+	// 					zip_code
+	// 					state
+	// 					street
+	// 					city
+	// 				}
+	// 				scheduleLine {
+	// 					quantity
+	// 					uom
+	// 					unitPrice
+	// 					totalAmount
+	// 				}
 	// 				currency
 	// 			}
 	// 		}
@@ -1510,22 +1436,87 @@ describe('Queries', () => {
 
 	// 	const { mutate } = createTestClient(server);
 	// 	const res = await mutate({
-	// 		mutation: UPDATE_ITEM,
+	// 		mutation: CREATE_ITEM,
 	// 		variables: {
 	// 			item: {
-	// 				id: '1',
-	// 				itemNo: '1',
-	// 				description: 'Corned Beef',
-	// 				quantity: 5,
-	// 				uom: 'kg',
-	// 				price: 2000,
+	// 				itemNo: '001',
+	// 				description: 'Beef',
+	// 				productId: '0001',
+	// 				quantity: 10,
+	// 				uom: 'kilograms',
+	// 				unitPrice: 1000,
+	// 				discount: 0.05,
+	// 				totalAmount: 10000,
+	// 				deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+	// 				scheduleLine: [
+	// 					{
+	// 						quantity: 10,
+	// 						uom: 'kilograms',
+	// 						deliveryDateAndTime: 'February 25,2020 4:30PM',
+	// 						unitPrice: 1000,
+	// 						totalAmount: 10000,
+	// 					},
+	// 				],
 	// 				currency: 'PHP',
 	// 			},
 	// 		},
 	// 	});
 
+	// 	console.log('testtt', res.errors);
+	// 	expect(res.errors).toBeUndefined();
+	// 	expect(itemMock.insert.mock.calls.length).toBe(1);
+
+	// 	expect(res.data).toMatchObject({
+	// 		createItem: {
+	// 			id: '1',
+	// 			itemNo: '001',
+	// 			description: 'Beef',
+	// 			productId: '0001',
+	// 			quantity: 10,
+	// 			uom: 'kilograms',
+	// 			unitPrice: 1000,
+	// 			discount: 0.05,
+	// 			totalAmount: 10000,
+	// 			deliveryAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+	// 			scheduleLine: [
+	// 				{
+	// 					id: '1',
+	// 					quantity: 10,
+	// 					uom: 'kilograms',
+	// 					deliveryDateAndTime: 'February 25,2020 4:30PM',
+	// 					unitPrice: 1000,
+	// 					totalAmount: 10000,
+	// 				},
+	// 			],
+	// 			currency: 'PHP',
+	// 		},
+	// 	});
 	// 	expect(res).toMatchSnapshot();
 	// });
+
+	it('update an item', async () => {
+		const UPDATE_ITEM = gql`
+			mutation i($item: UpdateItemInput!) {
+				updateItem(item: $item) {
+					id
+					supplierStatusItem
+				}
+			}
+		`;
+
+		const { mutate } = createTestClient(server);
+		const res = await mutate({
+			mutation: UPDATE_ITEM,
+			variables: {
+				item: {
+					id: '1',
+					supplierStatusItem: 'Dispatched',
+				},
+			},
+		});
+
+		expect(res).toMatchSnapshot();
+	});
 
 	it('delete an item', async () => {
 		const DELETE_ITEM = gql`
@@ -1545,26 +1536,66 @@ describe('Queries', () => {
 		expect(res).toMatchSnapshot();
 	});
 
-	// //Purchase Order Mutation
+	// //Purchase Order Mutations
+
 	// it('create a purchase order', async () => {
 	// 	const CREATE_PURCHASEORDER = gql`
 	// 		mutation createPO($purchaseOrder: PurchaseOrderInput) {
 	// 			createPurchaseOrder(purchaseOrder: $purchaseOrder) {
-	// 				id
-	// 				externalID
-	// 				status
-	// 				supplierStatus {
+	// 				purchaseOrderNo
+	// 				shipmentNo
+	// 				vendorAddress {
 	// 					id
+	// 					building_name
+	// 					street
+	// 					city
+	// 					state
+	// 					zip_code
 	// 				}
 	// 				supplier {
 	// 					id
-	// 					name
+	// 					supplierNo
+	// 					supplierName
+	// 					tin
+	// 					contactNumber
+	// 					contactPerson
 	// 					address {
 	// 						id
+	// 						building_name
+	// 						street
+	// 						city
+	// 						state
+	// 						zip_code
 	// 					}
 	// 				}
+	// 				documentDate
 	// 				items {
 	// 					id
+	// 					itemNo
+	// 					description
+	// 					quantity
+	// 					uom
+	// 					unitPrice
+	// 					currency
+	// 					deliveryAddress {
+	// 						id
+	// 					}
+	// 					totalAmount
+	// 					supplierStatusItem
+	// 					scheduleLine {
+	// 						id
+	// 						quantity
+	// 						uom
+	// 						unitPrice
+	// 						totalAmount
+	// 						deliveryDateAndTime
+	// 						deliveryStatus {
+	// 							id
+	// 							status
+	// 							dateCreated
+	// 							timeCreated
+	// 						}
+	// 					}
 	// 				}
 	// 			}
 	// 		}
@@ -1575,32 +1606,93 @@ describe('Queries', () => {
 	// 		mutation: CREATE_PURCHASEORDER,
 	// 		variables: {
 	// 			purchaseOrder: {
-	// 				externalID: '001',
-	// 				status: 'Pending',
-	// 				supplierStatus: [
-	// 					{
-	// 						status: 'Dispatched',
-	// 						dateCreated: 'February 14, 2020',
-	// 					},
-	// 				],
+	// 				purchaseOrderNo: '001',
+	// 				shipmentNo: '123',
 	// 				supplier: {
-	// 					name: 'Supplier Name-1',
-	// 					address: {
-	// 						building_name: 'building 1',
-	// 						city: 'city 1',
-	// 						street: 'street 1',
-	// 						state: 'ph',
-	// 						zip_code: '123',
-	// 					},
+	// 					supplierNo: '001',
+	// 					supplierName: 'Juan Dela Cruz',
+	// 					address: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+	// 					contactPerson: 'Basil Valdez',
+	// 					contactNumber: '1234567',
+	// 					tin: '12345',
 	// 				},
+	// 				postingDate: '03/03/20200',
+	// 				documentDate: '03/03/20200',
+	// 				vendorAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
 	// 				items: [
 	// 					{
-	// 						itemNo: '1',
-	// 						description: 'Corned Beef',
-	// 						quantity: 5,
+	// 						itemNo: '001',
+	// 						description: 'Beef',
+	// 						productId: '0001',
+	// 						quantity: 10,
 	// 						uom: 'kg',
-	// 						price: 2000,
+	// 						unitPrice: 1000,
+	// 						discount: 0.05,
+	// 						totalAmount: 9000,
+	// 						supplierStatusItem: 'Distpatched',
+	// 						deliveryAddress: {
+	// 							building_name: '002',
+	// 							street: 'Elmer',
+	// 							city: 'Celadon',
+	// 							state: 'Johto',
+	// 							zip_code: '123',
+	// 						},
+	// 						scheduleLine: [
+	// 							{
+	// 								quantity: 10,
+	// 								uom: 'kilograms',
+	// 								deliveryDateAndTime: 'February 25,2020 4:30PM',
+	// 								unitPrice: 1000,
+	// 								totalAmount: 10000,
+	// 							},
+	// 							{
+	// 								quantity: 10,
+	// 								uom: 'uom',
+	// 								deliveryDateAndTime: 'dateandtime',
+	// 								unitPrice: 1000,
+	// 								totalAmount: 10000,
+	// 							},
+	// 						],
 	// 						currency: 'PHP',
+	// 						dateUpdated: '',
+	// 						timeUpdated: '',
+	// 					},
+	// 					{
+	// 						itemNo: '002',
+	// 						description: 'Beef',
+	// 						productId: '0001',
+	// 						quantity: 10,
+	// 						uom: 'kg',
+	// 						unitPrice: 1000,
+	// 						discount: 0.05,
+	// 						totalAmount: 9000,
+	// 						supplierStatusItem: 'Distpatched',
+	// 						deliveryAddress: {
+	// 							building_name: '002',
+	// 							street: 'Elmer',
+	// 							city: 'Celadon',
+	// 							state: 'Johto',
+	// 							zip_code: '123',
+	// 						},
+	// 						scheduleLine: [
+	// 							{
+	// 								quantity: 10,
+	// 								uom: 'kilograms',
+	// 								deliveryDateAndTime: 'February 25,2020 4:30PM',
+	// 								unitPrice: 1000,
+	// 								totalAmount: 10000,
+	// 							},
+	// 							{
+	// 								quantity: 10,
+	// 								uom: 'uom',
+	// 								deliveryDateAndTime: 'dateandtime',
+	// 								unitPrice: 1000,
+	// 								totalAmount: 10000,
+	// 							},
+	// 						],
+	// 						currency: 'PHP',
+	// 						dateUpdated: '',
+	// 						timeUpdated: '',
 	// 					},
 	// 				],
 	// 			},
@@ -1611,40 +1703,147 @@ describe('Queries', () => {
 	// 	expect(purchaseOrderMock.insert.mock.calls.length).toBe(1);
 	// 	expect(res.data).toMatchObject({
 	// 		createPurchaseOrder: {
-	// 			id: '1',
-	// 			externalID: '001',
-	// 			status: 'Pending',
-	// 			supplierStatus: [
-	// 				{
-	// 					_id: '1',
-	// 					status: 'Dispatched',
-	// 					dateCreated: 'February 14, 2020',
-	// 				},
-	// 			],
+	// 			_id: '1',
+	// 			purchaseOrderNo: '001',
+	// 			shipmentNo: '123',
 	// 			supplier: {
 	// 				_id: '1',
-	// 				name: 'Supplier Name-1',
-	// 				address: {
-	// 					building_name: 'building 1',
-	// 					city: 'city 1',
-	// 					street: 'street 1',
-	// 					state: 'ph',
-	// 					zip_code: '123',
-	// 				},
+	// 				supplierNo: '001',
+	// 				supplierName: 'Juan Dela Cruz',
+	// 				address: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
+	// 				contactPerson: 'Basil Valdez',
+	// 				contactNumber: '1234567',
+	// 				tin: '12345',
 	// 			},
+	// 			postingDate: '03/03/20200',
+	// 			documentDate: '03/03/20200',
+	// 			vendorAddress: { building_name: '002', street: 'Elmer', city: 'Celadon', state: 'Johto', zip_code: '123' },
 	// 			items: [
 	// 				{
-	// 					id: '1',
-	// 					itemNo: '1',
-	// 					description: 'Corned Beef',
-	// 					quantity: 5,
+	// 					_id: '1',
+	// 					itemNo: '001',
+	// 					description: 'Beef',
+	// 					productId: '0001',
+	// 					quantity: 10,
 	// 					uom: 'kg',
-	// 					price: 2000,
+	// 					unitPrice: 1000,
+	// 					discount: 0.05,
+	// 					totalAmount: 9000,
+	// 					supplierStatusItem: 'Distpatched',
+	// 					deliveryAddress: {
+	// 						building_name: '002',
+	// 						street: 'Elmer',
+	// 						city: 'Celadon',
+	// 						state: 'Johto',
+	// 						zip_code: '123',
+	// 					},
+	// 					scheduleLine: [
+	// 						{
+	// 							quantity: 10,
+	// 							uom: 'kilograms',
+	// 							deliveryDateAndTime: 'February 25,2020 4:30PM',
+	// 							unitPrice: 1000,
+	// 							totalAmount: 10000,
+	// 						},
+	// 						{
+	// 							_id: '2',
+	// 							quantity: 10,
+	// 							uom: 'uom',
+	// 							deliveryDateAndTime: 'dateandtime',
+	// 							unitPrice: 1000,
+	// 							totalAmount: 10000,
+	// 						},
+	// 					],
 	// 					currency: 'PHP',
+	// 					dateUpdated: '',
+	// 					timeUpdated: '',
+	// 				},
+	// 				{
+	// 					itemNo: '002',
+	// 					description: 'Beef',
+	// 					productId: '0001',
+	// 					quantity: 10,
+	// 					uom: 'kg',
+	// 					unitPrice: 1000,
+	// 					discount: 0.05,
+	// 					totalAmount: 9000,
+	// 					supplierStatusItem: 'Distpatched',
+	// 					deliveryAddress: {
+	// 						building_name: '002',
+	// 						street: 'Elmer',
+	// 						city: 'Celadon',
+	// 						state: 'Johto',
+	// 						zip_code: '123',
+	// 					},
+	// 					scheduleLine: [
+	// 						{
+	// 							_id: '1',
+	// 							quantity: 10,
+	// 							uom: 'kilograms',
+	// 							deliveryDateAndTime: 'February 25,2020 4:30PM',
+	// 							unitPrice: 1000,
+	// 							totalAmount: 10000,
+	// 						},
+	// 						{
+	// 							_id: '2',
+	// 							quantity: 10,
+	// 							uom: 'uom',
+	// 							deliveryDateAndTime: 'dateandtime',
+	// 							unitPrice: 1000,
+	// 							totalAmount: 10000,
+	// 						},
+	// 					],
+	// 					currency: 'PHP',
+	// 					dateUpdated: '',
+	// 					timeUpdated: '',
 	// 				},
 	// 			],
 	// 		},
 	// 	});
 	// 	expect(res).toMatchSnapshot();
 	// });
+
+	it('update a purchase order', async () => {
+		const UPDATE_PURCHASEORDER = gql`
+			mutation i($item: UpdateItemInput!) {
+				updateItem(item: $item) {
+					id
+					adminStatus
+					supplierStatusHeader
+				}
+			}
+		`;
+
+		const { mutate } = createTestClient(server);
+		const res = await mutate({
+			mutation: UPDATE_PURCHASEORDER,
+			variables: {
+				item: {
+					id: '1',
+					adminStatus: 'Recieved',
+					supplierStatusHeader: 'Delivered',
+				},
+			},
+		});
+
+		expect(res).toMatchSnapshot();
+	});
+
+	it('delete a puchase order', async () => {
+		const DELETE_PURCHASEORDER = gql`
+			mutation i($id: ID!) {
+				deletePurchaseOrder(id: $id) {
+					id
+				}
+			}
+		`;
+
+		const { mutate } = createTestClient(server);
+		const res = await mutate({
+			mutation: DELETE_PURCHASEORDER,
+			variables: { id: '1' },
+		});
+
+		expect(res).toMatchSnapshot();
+	});
 });
