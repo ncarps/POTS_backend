99c2a9f135a15945ddb3811a0271af9a403bf5f8
iammarrkii <nabablitmarkangelo@gmail.com>
2020-03-17 09:15:33 +0800

    new end point get summary per status

--- server/src/graphql/typeDefs/purchaseOrder/index.ts
+++ server/src/graphql/typeDefs/purchaseOrder/index.ts
@@ -1,53 +1,54 @@
 import { gql } from 'apollo-server';
 
 const typeDefs = gql`
-	type PurchaseOrder {
-		id: ID!
-		purchaseOrderNo: String!
-		shipmentNo: String!
-		adminStatus: String
-		supplierStatusHeader: String
-		vendorAddress: Address!
-		supplier: Supplier!
-		documentDate: String
-		postingDate: String
-		items: [Item!]
-	}
+  type PurchaseOrder {
+    id: ID!
+    purchaseOrderNo: String!
+    shipmentNo: String!
+    adminStatus: String
+    supplierStatusHeader: String
+    vendorAddress: Address!
+    supplier: Supplier!
+    documentDate: String
+    postingDate: String
+    items: [Item!]
+  }
 
-	type Query {
-		purchaseOrder(id: ID!): PurchaseOrder
-		allPurchaseOrders: [PurchaseOrder]
-	}
+  type Query {
+    purchaseOrder(id: ID!): PurchaseOrder
+    allPurchaseOrders: [PurchaseOrder]
+    purchaseOrdersStatus(status: String): [PurchaseOrder]
+  }
 
-	type Mutation {
-		createPurchaseOrder(purchaseOrder: PurchaseOrderInput): PurchaseOrder
-		deletePurchaseOrder(id: ID!): PurchaseOrder
-		updatePurchaseOrder(purchaseOrder: UpdatePurchaseOrderInput): PurchaseOrder
-	}
+  type Mutation {
+    createPurchaseOrder(purchaseOrder: PurchaseOrderInput): PurchaseOrder
+    deletePurchaseOrder(id: ID!): PurchaseOrder
+    updatePurchaseOrder(purchaseOrder: UpdatePurchaseOrderInput): PurchaseOrder
+  }
 
-	input PurchaseOrderInput {
-		purchaseOrderNo: String!
-		shipmentNo: String!
-		adminStatus: String
-		supplierStatusHeader: String
-		vendorAddress: AddressInput!
-		documentDate: String
-		postingDate: String
-		supplier: SupplierInput!
-		items: [ItemInput!]!
-	}
+  input PurchaseOrderInput {
+    purchaseOrderNo: String!
+    shipmentNo: String!
+    adminStatus: String
+    supplierStatusHeader: String
+    vendorAddress: AddressInput!
+    documentDate: String
+    postingDate: String
+    supplier: SupplierInput!
+    items: [ItemInput!]!
+  }
 
-	input UpdatePurchaseOrderInput {
-		id: ID!
-		purchaseOrderNo: String
-		shipmentNo: String
-		adminStatus: String
-		supplierStatusHeader: String
-		documentDate: String
-		postingDate: String
-		supplier: UpdateSupplierInput
-		items: [UpdateItemInput]
-	}
+  input UpdatePurchaseOrderInput {
+    id: ID!
+    purchaseOrderNo: String
+    shipmentNo: String
+    adminStatus: String
+    supplierStatusHeader: String
+    documentDate: String
+    postingDate: String
+    supplier: UpdateSupplierInput
+    items: [UpdateItemInput]
+  }
 `;
 
 export default typeDefs;
