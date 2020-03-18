d14d261d6aff9149bcd86f3eecffe4e8d106ef2b
nat <nsebcarps09@gmail.com>
2020-03-18 10:42:24 +0800

    Update commons folder comments

--- server/src/commons/types.ts
+++ server/src/commons/types.ts
@@ -1,12 +1,12 @@
 export interface IDBModel<T> {
-	insert: (input: any) => PromiseLike<T>;
-	getById: (input: any) => PromiseLike<T>;
-	//Special queries
-	getAllBySupplierStatus: (input: any) => PromiseLike<T>;
-	getAllByItem: (input: any) => PromiseLike<T>;
-	getAllByScheduleLine: (input: any) => PromiseLike<T>;
-	/////////////////
-	getAll: () => PromiseLike<T>;
-	deleteById: (input: any) => PromiseLike<T>;
-	updateById: (input: any) => PromiseLike<T>;
+  insert: (input: any) => PromiseLike<T>;
+  getById: (input: any) => PromiseLike<T>;
+  //Non-Generic Functions
+  getAllBySupplierStatus: (input: any) => PromiseLike<T>;
+  getAllByItem: (input: any) => PromiseLike<T>;
+  getAllByScheduleLine: (input: any) => PromiseLike<T>;
+  /////////////////
+  getAll: () => PromiseLike<T>;
+  deleteById: (input: any) => PromiseLike<T>;
+  updateById: (input: any) => PromiseLike<T>;
 }
